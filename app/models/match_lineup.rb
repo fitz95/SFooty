cf1f3b85e1c9800b6e2d56e08f1e52de5c2181a0
fitz95 <fitzgeraldnsong@gmail.com>
2023-09-06 19:33:49 +0100

    update match_lineup model

--- app/models/match_lineup.rb
+++ app/models/match_lineup.rb
@@ -1,10 +1,11 @@
 class MatchLineup < ApplicationRecord
     belongs_to :match
+    belongs_to :team
     belongs_to :user
     belongs_to :formation
-    has_many :lineup_positions, through: :lineup_players
-    has_many :lineup_substitute_options, dependent: :destroy
     has_many :lineup_players
+    has_many :players, through: :lineup_players
+    has_many :lineup_substitute_options, dependent: :destroy
     has_many :substitute_players, through: :lineup_substitute_options, source: :substitute_player
     has_many :match_substitutions
 end
