404feb842f8fafae3937ccf34d27adbe5d3fee98
fitz95 <fitzgeraldnsong@gmail.com>
2023-09-06 22:13:34 +0100

    update post, refree, and stadium as well as relationship and models

--- app/models/referee.rb
+++ app/models/referee.rb
@@ -1,4 +1,5 @@
 class Referee < ApplicationRecord
     belongs_to :user
     belongs_to :league
+    has_many :matches, dependent: :nullify
 end
--- app/models/stadium.rb
+++ app/models/stadium.rb
@@ -1,4 +1,5 @@
 class Stadium < ApplicationRecord
-    belongs_to :team
+    belongs_to :team, optional: true
+    has_many :matches, dependent: :nullify
     belongs_to :user
 end
