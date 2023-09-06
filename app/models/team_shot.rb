8097b2eecde3d4b2f77d379038294eeeebda52e1
fitz95 <fitzgeraldnsong@gmail.com>
2023-09-06 22:21:50 +0100

    update team_shots and team as well as relationship and models

--- app/models/team_shot.rb
+++ app/models/team_shot.rb
@@ -2,4 +2,7 @@ class TeamShot < ApplicationRecord
     belongs_to :team
     belongs_to :match
     belongs_to :user
+
+    # Example: A scope to find team shots on target in a specific match
+    scope :shots_on_target_in_match, ->(match_id) { where(match_id: match_id).sum(:shots_on_target) }
 end
