f84e0ac355d5e4db59c125919a8c88a39aebe40a
fitz95 <fitzgeraldnsong@gmail.com>
2023-09-06 11:05:09 +0100

    add model associations for follow, formation, game_week, league and lineup_player tables

--- app/models/league.rb
+++ app/models/league.rb
@@ -1,3 +1,5 @@
 class League < ApplicationRecord
   belongs_to :user
+  has_many :matches, dependent: :destroy, foreign_key: 'league_id', inverse_of: :league, counter_cache: : true
+  has_many :game_weeks, dependent: :destroy
 end
--- app/models/lineup_player.rb
+++ app/models/lineup_player.rb
@@ -1,7 +1,10 @@
 class LineupPlayer < ApplicationRecord
-    belongs_to :lineup_position
-    belongs_to :player
+    belongs_to :match
+    belongs_to :team
+    belongs_to :formation
     belongs_to :user
-    belongs_to :match_lineup
+    has_many :lineup_players, dependent: :destroy, foreign_key: :match_lineup_id, inverse_of: :match_lineup, counter_cache: true
+    has_many :lineup_substitute_options, dependent: :destroy
+    has_many :match_substitutions, dependent: :destroy
 end
 
