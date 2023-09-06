1cb910a73c1e14660f511744f24a3657cda0ad8b
fitz95 <fitzgeraldnsong@gmail.com>
2023-09-06 18:47:40 +0100

    modified model for lineup_position and lineuop_substitute_option

--- app/models/match_lineup.rb
+++ app/models/match_lineup.rb
@@ -2,6 +2,7 @@ class MatchLineup < ApplicationRecord
     belongs_to :match
     belongs_to :user
     belongs_to :formation
+    has_many :lineup_positions, through: :lineup_players
     has_many :lineup_substitute_options, dependent: :destroy
     has_many :lineup_players
     has_many :substitute_players, through: :lineup_substitute_options, source: :substitute_player
--- app/models/team.rb
+++ app/models/team.rb
@@ -2,4 +2,5 @@ class Team < ApplicationRecord
   belongs_to :league
   belongs_to :user
   has_many :players
+  belongs_to :league
 end
