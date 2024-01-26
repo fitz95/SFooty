3f4f6871161351a14d878b638647f49fa1243bd4
fitz95 <fitzgeraldnsong@gmail.com>
2024-01-26 07:01:03 +0100

    format the ability.rb file better and add more tables the normal user can read

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,26 +4,46 @@ class Ability
   def initialize(user)
     user ||= User.new # Guest user (not logged in)
 
-    # Anyone with an account can follow any account
     can :create, Follow if user.persisted?
 
-    # Abilities for different roles
     if user.super_admin?
       can :manage, :all
     elsif user.admin?
-      can :manage, [Formation, GameWeek, League, LineupPlayer, LineupPosition, LineupSubstituteOption, Match, MatchOfficial, Player, Referee, Trophy, Stadium, Team, User, Transfer, TrophyPlayer, MatchEvent, MatchGoal, MatchLineup, MatchOfficialForMatch, MatchShot, MatchSubstitute, PlayerPosition, PlayerInjury, PlayerStat, TeamStat]
+      can :manage, [
+        Formation, GameWeek, League, LineupPlayer, LineupPosition,
+        LineupSubstituteOption, Match, MatchOfficial, Player, Referee,
+        Trophy, Stadium, Team, User, Transfer, TrophyPlayer, MatchEvent,
+        MatchGoal, MatchLineup, MatchOfficialForMatch, MatchShot,
+        MatchSubstitute, PlayerPosition, PlayerInjury, PlayerStat, TeamStat
+      ]
     elsif user.data_collector?
-      can :manage, [Formation, GameWeek, LineupPlayer, LineupPosition, LineupSubstituteOption, MatchEvent, MatchGoal, MatchLineup, MatchPlayer, MatchOfficial, MatchOfficialForMatch, MatchShot, MatchSubstitute, Player, Stadium, TrophyPlayer, PlayerPosition, PlayerInjury, PlayerStat, TeamStat ]
+      can :manage, [
+        Formation, GameWeek, LineupPlayer, LineupPosition,
+        LineupSubstituteOption, MatchEvent, MatchGoal, MatchLineup,
+        MatchPlayer, MatchOfficial, MatchOfficialForMatch, MatchShot,
+        MatchSubstitute, Player, Stadium, TrophyPlayer, PlayerPosition,
+        PlayerInjury, PlayerStat, TeamStat
+      ]
     elsif user.client?
-      can :read, [Follow, Formation, GameWeek, League, LineupPlayer, LineupPosition, LineupSubstituteOption, Match, Player, Referee, Trophy]
+      can :read, [
+        Follow, Formation, GameWeek, League, LineupPlayer, LineupPosition,
+        LineupSubstituteOption, Match, Player, Referee, Trophy
+      ]
       can :create, Follow if user.persisted?
       # Define more abilities as needed for clients
     else
-      can :read, [Follow, Formation, GameWeek, LineupPlayer, LineupPosition, LineupSubstituteOption, Match, Player, Referee, Trophy, ...]
+      can :read, [
+        Follow, Formation, GameWeek, LineupPlayer, LineupPosition,
+        LineupSubstituteOption, Match, Player, Referee, Trophy, Stadium, Team,
+        User, Transfer, TrophyPlayer, MatchEvent, MatchGoal, MatchLineup,
+        MatchOfficialForMatch, MatchShot, MatchSubstitute, PlayerPosition,
+        PlayerInjury, PlayerStat, TeamStat, League, GameWeek, MatchOfficial
+      ]
       can :create, Follow if user.persisted?
       # Default abilities for guests
     end
   end
 end
+
 
 
