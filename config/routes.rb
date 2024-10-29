d45c498feb2ab500ac670b9b942bb0ac6dc4799d
Fitz Gerald <fitzgeraldnsong@gmail.com>
2024-10-29 07:35:49 +0000

    modified routes to account for update

--- config/routes.rb
+++ config/routes.rb
@@ -1,32 +1,30 @@
-Rails.application.routes.draw do
-  mount Rswag::Ui::Engine => '/api-docs'
-  mount Rswag::Api::Engine => '/api-docs'
-  root to: redirect('/api-docs')
-  apipie
-  namespace :api do
-    namespace :v1 do
-      # Devise routes
-      devise_for :users, controllers: {
-        sessions: 'api/v1/sessions',
-        registrations: 'api/v1/registrations',
-        passwords: 'api/v1/passwords'
-      }
-      # Registration route
-      #other routes
-      # User routes
-      resources :users do
-        resources :leagues, only: [:index, :show, :create, :destroy] do
-          resources :teams, only: [:index, :show, :create, :destroy] do 
-            resources :players, only: [:index, :show, :create, :destroy]
-            resources :trophies, only: [:index, :show, :create, :destroy]
-            resources :stadiums, only: [:index, :show, :create, :destroy]
-          end 
+namespace :api do
+  namespace :v1 do
+    # Devise routes
+    devise_for :users, controllers: {
+      sessions: 'api/v1/sessions',
+      registrations: 'api/v1/registrations',
+      passwords: 'api/v1/passwords'
+    }
+
+    # User routes
+    resources :users do
+      resources :leagues do
+        # Include the update action here
+        member do
+          patch :update
         end
-        resources :posts, only: [:index, :show, :create, :destroy]
-        resources :match_officials, only: [:index, :show, :create, :destroy]
-        resources :player_transfers, only: [:index, :show, :create, :destroy]
-        resources :player_injuries, only: [:index, :show, :create, :destroy]
+        
+        resources :teams, only: [:index, :show, :create, :destroy] do 
+          resources :players, only: [:index, :show, :create, :destroy]
+          resources :trophies, only: [:index, :show, :create, :destroy]
+          resources :stadiums, only: [:index, :show, :create, :destroy]
+        end 
       end
+      resources :posts, only: [:index, :show, :create, :destroy]
+      resources :match_officials, only: [:index, :show, :create, :destroy]
+      resources :player_transfers, only: [:index, :show, :create, :destroy]
+      resources :player_injuries, only: [:index, :show, :create, :destroy]
     end
   end
 end
