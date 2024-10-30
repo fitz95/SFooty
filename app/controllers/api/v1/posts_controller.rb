57ec1a89fee8a3d966dddefa04dca3a040a06532
fitz95 <fitzgeraldnsong@gmail.com>
2024-10-30 10:45:49 +0100

    post controller modify as well as migration

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,62 +1,60 @@
 # app/controllers/api/v1/posts_controller.rb
 class Api::V1::PostsController < ApplicationController
-    before_action :authenticate_request
-    before_action :set_user
-    before_action :set_post
-    load_and_authorize_resource
-    before_action :set_post, only: %i[show edit update destroy]
-  
-    api :GET, '/v1/posts', 'Get all posts'
-    def index
-      @posts = Post.all
-      render json: @posts
-    end
-  
-    api :GET, '/v1/posts/:id', 'Get post by id'
-    param :id, :number, desc: 'id of the requested post', required: true
-    error code: 404, desc: 'Post not found!'
-    def show
-      render json: @post
-    end
-  
-    api :POST, '/v1/posts', 'Create a new post'
-    def create
-      @post = current_user.posts.new(post_params)
-  
-      if @post.save
-        render json: @post, status: :created
-      else
-        render json: @post.errors, status: :unprocessable_entity
-      end
-    end
-  
-    api :PATCH, '/v1/posts/:id', 'Update post by id'
-    def update
-      if @post.update(post_params)
-        render json: @post
-      else
-        render json: @post.errors, status: :unprocessable_entity
-      end
-    end
-  
-    api :DELETE, '/v1/posts/:id', 'Delete post by id'
-    def destroy
-      @post.destroy
-      head :no_content
-    end
-  
-    private
-  
-    def set_post
-      @post = Post.find(params[:id])
-    end
+  before_action :authenticate_request
+  before_action :set_user
+  before_action :set_post, only: %i[show edit update destroy]
+  load_and_authorize_resource
 
-    def set_user
-        @user = User.find(params[:user_id])
-    end
-  
-    def post_params
-      params.require(:post).permit(:title, :body, :photo, :status, :team_id, :player_id, :league_id)
+  api :GET, '/api/v1/users/:user_id/posts', 'Get all posts for a user'
+  def index
+    @posts = @user.posts
+    render json: @posts
+  end
+
+  api :GET, '/api/v1/users/:user_id/posts/:id', 'Get a post by id for a user'
+  param :id, :number, desc: 'ID of the requested post', required: true
+  error code: 404, desc: 'Post not found!'
+  def show
+    render json: @post
+  end
+
+  api :POST, '/api/v1/users/:user_id/posts', 'Create a new post for a user'
+  def create
+    @post = @user.posts.new(post_params)
+    
+    if @post.save
+      render json: @post, status: :created
+    else
+      render json: @post.errors, status: :unprocessable_entity
     end
   end
+
+  api :PATCH, '/api/v1/users/:user_id/posts/:id', 'Update a post by id for a user'
+  def update
+    if @post.update(post_params)
+      render json: @post
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
+  end
+
+  api :DELETE, '/api/v1/users/:user_id/posts/:id', 'Delete a post by id for a user'
+  def destroy
+    @post.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
   
+  def set_post
+    @post = @user.posts.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body, :photo, :status, :team_id, :player_id, :video, :additional_images, :league_id)
+  end
+end
