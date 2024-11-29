5b91a2c7b4bd4ced3c446fbf1abcf9216fa2e1ae
fitz95 <fitzgeraldnsong@gmail.com>
2024-11-29 22:49:27 +0100

    add migration fix for competition table

--- db/migrate/20241107065053_add_details_to_competitions.rb
+++ db/migrate/20241107065053_add_details_to_competitions.rb
@@ -7,7 +7,7 @@ class AddDetailsToCompetitions < ActiveRecord::Migration[7.0]
     add_column :competitions, :end_date, :date
     add_column :competitions, :status, :string
     add_column :competitions, :location, :string
-    add_column :competitions, :description, :text
+    change_column :competitions, :description, :text
 
   end
 end
