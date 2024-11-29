d89dd98810758ebc83f5666852c9126aa11946e6
fitz95 <fitzgeraldnsong@gmail.com>
2024-11-29 22:34:37 +0100

    fixed some migration at the add detail competition table

--- db/migrate/20241107065053_add_details_to_competitions.rb
+++ db/migrate/20241107065053_add_details_to_competitions.rb
@@ -1,6 +1,8 @@
 class AddDetailsToCompetitions < ActiveRecord::Migration[7.0]
   def change
-    add_reference :competitions, :user, foreign_key: true
+    unless column_exists?(:competitions, :user_id)
+      add_column :competitions, :user_id, :integer
+    end
     add_column :competitions, :start_date, :date
     add_column :competitions, :end_date, :date
     add_column :competitions, :status, :string
